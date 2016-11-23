3650b18603b1473029d856189508ad7099dcb46f
Le Huy <huy.le.tg1996@gmail.com>
2016-11-23 15:29:00 +0700

    .
    thêm name

---
+++ postnew.php
@@ -0,0 +1,16 @@
+<?php
+
+$ten = $_GET["ten"];
+$sodienthoai = $_GET["sodienthoai"];
+$diachi = $_GET["diachi"];
+$tieude = $_GET["tieude"];
+$thongtin = $_GET["thongtin"];
+$money = $_GET["money"];
+//include "data_access_helper.php";
+//$db = new DataAccessHelper;
+//$db->connect();
+
+//$db->executeNonQuery("INSERT INTO   
+//$db->close(); 
+//echo "ten";
+?> 
