3091fa3a1229e71655dfd42b9b19fb6845feb4c1
haithanh079 <haithanh079@gmail.com>
2016-12-14 15:42:37 +0700

    backend generate

---
+++ YiiBasic/views/admin/_form.php
@@ -0,0 +1,31 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Product */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="product-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'userID')->textInput() ?>
+
+    <?= $form->field($model, 'ProductName')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'Price')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'Category')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'ImageURL')->textInput(['maxlength' => true]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
