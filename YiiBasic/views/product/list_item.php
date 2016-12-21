f62f86c28992a82e79116e915e22395cdf607ae1
haithanh079 <haithanh079@gmail.com>
2016-12-21 22:28:11 +0700

    reponsive

--- YiiBasic/views/product/list_item.php
+++ YiiBasic/views/product/list_item.php
@@ -3,7 +3,7 @@
 use yii\helpers\Html;
 use yii\helpers\Url;
 ?>
-<article class="item col-md-9 col-sm-9 col-xs-9" data-key="<?= $model->ID; ?>">
+<article class="item col-md-9 col-sm-12 col-xs-12" data-key="<?= $model->ID; ?>">
     <h4 class="title">
     <?= Html::a(Html::encode($model->ProductName), ['view', 'id' => $model->ID]) ?>
     </h4>
